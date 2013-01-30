7576228ac616e5bf76792305788bbe737e025030
Matthieu Billard <Matt@Matthieus-MacBook-Air.local>
2013-01-29 21:24:30 -0500

    db install

--- web.js
+++ web.js
@@ -1,3 +1,11 @@
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+redis.set('foo', 'bar');
+
+redis.get('foo', function(err, value) {
+  console.log('foo is: ' + value);
+});
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
